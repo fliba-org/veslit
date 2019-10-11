95c4dbb00bff5ec9e7729f7cf3a1acb761295c72
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2019-10-11 14:17:25 -0300

    frontend: add state for socketId

--- frontend/src/match-selection.js
+++ frontend/src/match-selection.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import openSocket from 'socket.io-client';
 import config from './config';
 
 const socket = openSocket(config.host);
 
 function MatchSelection() {
-    return 'Hi there';
+    const [socketId, updateSocketId] = useState('');
+
+    socket.on('socketId', id => { updateSocketId(id) })
+    return <div>Socket id: { socketId }</div>;
 }
 
 export default MatchSelection;
