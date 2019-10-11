94814f3edbfa2ca2c2071881801fce0ae20e8f22
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2019-10-11 14:16:23 -0300

    frontend: <MatchSelect />: use socket.io-client

--- frontend/src/match-selection.js
+++ frontend/src/match-selection.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import openSocket from 'socket.io-client';
+import config from './config';
+
+const socket = openSocket(config.host);
 
 function MatchSelection() {
     return 'Hi there';
