45b226f783b6fb79bbc239034ba03b8d18d0c420
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2019-10-13 04:14:04 -0300

    frontend: MatchSelection: use state for socket
    The socket have to properties: connected and createdMatch. Connected is
    used for displaying a warning message or the available matches on
    server and createdMatch is used for displaying or not the create match
    button.

--- frontend/src/match-selection.js
+++ frontend/src/match-selection.js
@@ -5,6 +5,15 @@ import config from './config';
 const socket = io(config.host);
 
 function MatchSelection() {
+    const [socketState, updateSocketState] = useState({
+        connected: false,
+        createdMatch: false,
+    });
+
+    socket.on('updateSocketState', s => {
+        updateSocketState(s)
+    });
+
 
     return (<div>
     </div>);
